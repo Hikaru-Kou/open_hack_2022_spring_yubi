29c707947d6151315f66de9c029d248b4883c2f9
sayyyyyy <d958956a6b650@gmail.com>
2023-02-13 21:59:52 +0900

    feat: フロント側環境構築完了

---
+++ frontend/src/nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+})
