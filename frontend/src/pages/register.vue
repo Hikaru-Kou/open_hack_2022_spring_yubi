6680f623ce00b5f3a124156e3daff939f09c5d54
sayyyyyy <d958956a6b650@gmail.com>
2023-02-16 11:56:21 +0900

    feat: #4 register機能作成

--- frontend/src/pages/register.vue
+++ frontend/src/pages/register.vue
@@ -1,9 +1,34 @@
 <template>
     <span>メールアドレス</span>
-    <input type="text">
+    <input type="text" v-model="email">
     <span>パスワード</span>
-    <input type="text">
+    <input type="text" v-model="password">
     <span>再パスワード</span>
-    <input type="text">
-    <button>Register</button>
+    <input type="text" v-model="rePassword">
+    <button v-on:click="registerUser">Register</button>
 </template>
+
+<script setup lang="ts">
+const email = ref('')
+const password = ref('')
+const rePassword = ref('')
+const API_URL = ''
+
+const registerUser = () => {
+    const postData = {
+        email: email,
+        password: password,
+        rePassword: rePassword
+    }
+    const registerController = useFetch(API_URL+ '/posts', {
+        method: 'POST',
+        body: postData,
+        headers:{
+        }
+    })
+    .then((e: any) => {
+        console.log(e)
+        navigateTo('/leisureButton')
+    })
+}
+</script>
