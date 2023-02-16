820e041bbfa72e2ffb435f097e4034a4c6c83fbf
sayyyyyy <d958956a6b650@gmail.com>
2023-02-16 16:59:34 +0900

    feat: #7 leisurelist取得機能の作成

--- frontend/src/pages/leisureList.vue
+++ frontend/src/pages/leisureList.vue
@@ -1,7 +1,30 @@
 <template>
     <p>暇リスト</p>
     <ul>
-        <li>暇つぶし１</li>
+        <li v-for="leisure in leisureList">{{ leisure }}</li>
         <li>暇つぶし２</li>
     </ul>
 </template>
+
+<script setup lang="ts">
+const API_URL = ''
+const leisureList = []
+const postData = {
+    userId: ''
+}
+
+const getLeisureList = () => {
+    const leisureListController = useFetch(API_URL+ '/posts', {
+        method: 'POST',
+        body: postData,
+        headers:{
+        }
+    })
+    .then((e: any) => {
+        console.log(e)
+        // leisureList = e
+    })
+}
+
+getLeisureList()
+</script>
