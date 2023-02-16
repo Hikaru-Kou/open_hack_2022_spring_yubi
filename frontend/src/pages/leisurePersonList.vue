17d2b8d6fc8a6eddf5ca1b399189edd2f8a31057
sayyyyyy <d958956a6b650@gmail.com>
2023-02-16 16:45:33 +0900

    feat: #6 暇な人一覧取得機能の作成

--- frontend/src/pages/leisurePersonList.vue
+++ frontend/src/pages/leisurePersonList.vue
@@ -4,4 +4,31 @@
         <li>ユーザー１</li>
         <li>ユーザー２</li>
     </ul>
+    <
 </template>
+
+<script setup lang="ts">
+const API_URL = ''
+const availableUsers: string[] = []
+const postData = {
+    userId: ''
+}
+
+const getAvailableUsers = () => {
+    const availableUserController = useFetch(API_URL+ '/posts', {
+        method: 'POST',
+        body: postData,
+        headers:{
+        }
+    })
+    .then((e: any) => {
+        console.log(e)
+        // availableUsers = e
+    })
+}
+
+const goToAddFriendPage = () => {
+    navigateTo('/addFriend')
+}
+getAvailableUsers()
+</script>
