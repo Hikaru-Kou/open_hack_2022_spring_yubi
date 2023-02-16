f92cd94c649234d9321e102890064a6d8e138790
sayyyyyy <d958956a6b650@gmail.com>
2023-02-16 11:27:23 +0900

    top画面機能追加

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-    <button>新規登録</button>
-    <button>ログイン</button>
+    <button v-on:click="goToRegistrationPage">新規登録</button>
+    <button v-on:click="goToLoginPage">ログイン</button>
 </template>
 
 <script setup lang="ts">
+const goToRegistrationPage = () => {
+    navigateTo('/register')
+}
+
+const goToLoginPage = () => {
+    navigateTo('/login')
+}
 </script>
